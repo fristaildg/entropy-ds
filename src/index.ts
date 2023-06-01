13269d39b71efbdc51e96b9bf17ac594fbb5904e
luis.fernandez <luis.fernandez@prt.manomano.com>
2023-06-01 18:12:49 +0200

    export all components

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export { Button } from './components/Button';
 export { ThemeProvider } from './components/ThemeProvider';
 export { Text } from './components/Text';
+export { Icon, IconsProvider } from './components/Icon';
+export { Input } from './components/Input';
+export { Radio } from './components/Radio';
+export { Textarea } from './components/Textarea';
